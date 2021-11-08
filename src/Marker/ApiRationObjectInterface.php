df5c364802d6fdb133f1fb6fdcd48a3ac3d894ff
Lebedenko Nikolay <totx@ya.ru>
2021-11-08 06:35:23 +0300

    Improvement ApiRationObjectInterface

--- src/Marker/ApiRationObjectInterface.php
+++ src/Marker/ApiRationObjectInterface.php
@@ -7,7 +7,7 @@ namespace YaPro\ApiRation\Marker;
 use YaPro\MarkerInterfaces\Base\ValueObjectInterface;
 
 /**
- * Объекты имплементирующие данные интерфейс могут быть автоматически преобразованы из/в json при HTTP-запросе.
+ * Объекты классов имплементирующих данный интерфейс могут быть автоматически преобразованы из/в json при HTTP-запросе.
  */
 interface ApiRationObjectInterface extends ValueObjectInterface
 {
